0d55f8014b4e0547620e25814d3bfd9e669cb9ed
Tisha Dey <tishadey2025@outlook.com>
2025-12-16 16:52:15 +0600

    :tada: operator commit

--- operator.js
+++ operator.js
@@ -4,4 +4,20 @@ var total =x+y+8;
 
 console.log(total);
   
-var minus = total
+var minus = total -10;
+console.log(minus);
+
+var maltiple = minus * 2;
+
+console.log(maltiple);
+
+var divide = maltiple / x;
+
+console.log(divide);
+
+
+
+
+
+
+
