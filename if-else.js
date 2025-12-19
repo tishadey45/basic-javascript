e72685ea6e86b53c52f818356272474e30bbaa52
Tisha Dey <tishadey2025@outlook.com>
2025-12-19 22:48:58 +0600

    :tada: if-else work done

---
+++ if-else.js
@@ -0,0 +1,13 @@
+// var x =25;
+// if (x > 10){
+// console.log("x 10 cheye boro");
+// }else{
+// console.log("x 10 cheye coto");
+// }
+
+var abc =40;
+if (abc === 30){
+    console.log("eqal to 30");
+}else{
+    console.log("not eqal to 30");
+}
