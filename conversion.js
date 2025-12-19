9d2862e16a443e2e5b1c4618ebbb1eb0b8b32cbf
Tisha Dey <tishadey2025@outlook.com>
2025-12-19 21:23:49 +0600

    :tada: conversion work done

---
+++ conversion.js
@@ -0,0 +1,26 @@
+// var nummber = 14.243;
+// var x = "19.234";
+// var y ="20";
+
+// console.log(parseInt(nummber));
+// var num = parseFloat(x);
+// console.log(num);
+// var z = parseInt(y);
+// console.log(z);
+
+// console.log(3+4);
+// console.log(true+4);
+// console.log(4+"4"); //string
+
+// var x = 4.5832345;
+// console.log(typeof x);
+
+// var convert = x.toFixed(3);
+// console.log(convert);
+// console.log(typeof convert);
+
+// console.log(5 + parseFloat(convert));
+
+console.log(isNaN(29));
+console.log(isNaN("29"));
+console.log(isNaN("abc"));
