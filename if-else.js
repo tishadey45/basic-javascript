7f00b5a07da269d789d09a38290669870efb5b41
Tisha Dey <tishadey2025@outlook.com>
2025-12-21 08:33:28 +0600

    :tada: if-else er logical done

--- if-else.js
+++ if-else.js
@@ -11,3 +11,27 @@ if (abc === 30){
 }else{
     console.log("not eqal to 30");
 }
+
+//logical operator && ||
+
+var hour = 14;
+if (hour >= 6 && hour < 12){
+    console.log("good morning");
+}else{
+    if(hour >= 12 && hour < 18){
+        console.log("good afternoon!");
+    }else{
+        console.log("good eveing!");
+    }
+}
+
+//logical or
+
+var x = 7;
+//x er man jodi 5 er choto hoy othoba 10 er boro hoy tahole true hobe
+
+if(x>5 || x>10){
+    console.log("true");
+}else{
+    console.log("false");
+}
