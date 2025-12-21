7b5197c226efd689a8eae7411c25afe000dcbdb1
Tisha Dey <tishadey2025@outlook.com>
2025-12-21 09:35:43 +0600

    :tada: ternari work done

---
+++ ternari.js
@@ -0,0 +1,23 @@
+// var y = 10;
+// var massage;
+
+// if(y>5){
+    // massage ="bigger than 5";
+// }else{
+    // massage ="lese than 5";
+// }
+
+// condition ? true:false
+// massage =y>5 ? "bigger than 5" : "lese than 5";
+
+// console.log(massage);
+// var sunyday =true;
+// console.log(!sunyday);
+
+// console.log(!(10 === 1)); //not dile ultano jay
+
+// difference of == and ===
+// == compares only value. === compares value with datatype
+
+// console.log(10 == 10);
+console.log(10 === "10");
